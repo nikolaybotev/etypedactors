d7454351603eba8af737e34f9b7ca6e091ae9be0
Nikolay Botev <bono8106@yahoo.com>
2011-12-03 17:35:48 -0800

    consolidate

--- src/main/scala/org/etypedactors/akka/AkkaEtypedActorFactory.scala
+++ src/main/scala/org/etypedactors/akka/AkkaEtypedActorFactory.scala
@@ -5,10 +5,23 @@ import akka.actor.ActorRef
 import org.etypedactors.ActorType
 import org.etypedactors.ActorFactory
 import org.etypedactors.ActorWithProxy
+import org.etypedactors.ETypedActorMessageHandler
 
 class AkkaActorType(makeActorRef: => ActorRef) {
   lazy val actorRef = makeActorRef
   def !(message: Any) = actorRef.! (message) (null) // do not care about Akka sender
+}
+
+private class AkkaEtypedActor(protected val myself: ActorWithProxy[AkkaActorType], makeImpl: => Any) extends Actor with ETypedActorMessageHandler {
+
+  lazy protected val impl = makeImpl
+
+  override def preStart() {
+    impl // Lazily invoke the factory
+  }
+
+  def receive = messageHandler
+
 }
 
 class AkkaEtypedActorFactory extends ActorFactory[AkkaActorType] {
